84e749894393e977fbb66feb04accc14dceec9b8
Yu Li <YLiLarry@gmail.com>
2016-04-20 23:01:44 -0400

    adding tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -2,10 +2,11 @@ import Test.Hspec
 
 import VC.Update
 import System.Directory
+import Control.Monad.Trans.Reader
 
 testURLConfig = URLConfig {
-     serverConfigFingerprintURL = String
-   , serverConfigDirectoryURL   = String
+     serverConfigFingerprintURL = "http://localhost:8000/api/current/config/fingerprints"
+   , serverConfigDirectoryURL   = "http://localhost:8000/api/current/config/fingerprints/"
    , localConfigDirectoryURL    = "./test/test-config-dir/"
 }
 
@@ -13,6 +14,6 @@ main :: IO ()
 main = hspec $ do
    describe "first" $ do
       it "" $ do
-         print =<< localConfigFingerprints 
+         print =<< runReaderT serverLocalDiff testURLConfig
          
 
